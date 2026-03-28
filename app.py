1f75002b5d8427405bf1d90408b0884ee8af8097
glebe <glebegor1982@gmail.com>
2026-03-28 10:38:34 +0300

    6 commit

--- app.py
+++ app.py
@@ -262,13 +262,13 @@ def services():
 #     app.run(debug=True, port=8000)
 
 if __name__ == '__main__':
-    # 1. Запуск бота в отдельном "демоне"
-    import threading
-    try:
-        t = threading.Thread(target=run_bot, daemon=True)
-        t.start()
-    except Exception as e:
-        print(f"Ошибка запуска бота: {e}")
+    # # 1. Запуск бота в отдельном "демоне"
+    # import threading
+    # try:
+    #     t = threading.Thread(target=run_bot, daemon=True)
+    #     t.start()
+    # except Exception as e:
+    #     print(f"Ошибка запуска бота: {e}")
 
     # 2. Жестко ставим порт 80
     # На Amvera это самый стабильный вариант для избавления от 503/502
